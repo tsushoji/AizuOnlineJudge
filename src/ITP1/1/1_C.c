fb9c4287733d6f2acef1f20fb52916837977aaaf
tsusho <s.tuji3007sho@gmail.com>
2021-03-23 23:19:52 +0900

    ITP1_1追加#1

---
+++ src/ITP1/1/1_C.c
@@ -0,0 +1,13 @@
+#include "1.h"
+
+void rectangleLengthDisplay(void)
+{
+    int x;
+	int y;
+	int area;
+	int circumferential_length;
+	scanf("%d %d", &x, &y);
+	area = x * y;
+	circumferential_length = 2 * (x + y);
+	printf("%d %d\n", area, circumferential_length);
+}
